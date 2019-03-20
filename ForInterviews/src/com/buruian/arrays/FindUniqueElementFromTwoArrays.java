6bc999873b9a2095b213b645653eff536fde21d7
Valeriu Buruian <valeraburuian@mail.ru>
2019-03-20 16:31:06 +0200

    Added a new approach which think is the best one

--- ForInterviews/src/com/buruian/arrays/FindUniqueElementFromTwoArrays.java
+++ ForInterviews/src/com/buruian/arrays/FindUniqueElementFromTwoArrays.java
@@ -2,8 +2,10 @@ package com.buruian.arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class FindUniqueElementFromTwoArrays {
 
@@ -26,7 +28,7 @@ public class FindUniqueElementFromTwoArrays {
 
 		}
 
-		System.out.println(unique);
+		System.out.println("First approach - searched element is : " +unique);
 	}
 
 	public static void findElementTheBetterApproach(Integer[] biggestArray, Integer[] secondArray) {
@@ -34,13 +36,30 @@ public class FindUniqueElementFromTwoArrays {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(biggestArray));
 		HashSet<Integer> set = new HashSet<Integer>(list);
 		set.removeAll(Arrays.asList(secondArray));
-		System.out.println(set);
+		System.out.println("Second approach - searched element is :" +set);
 
+	}
+
+	public static void findElementUsingMap(Integer[] biggestArray, Integer[] secondArray) {
+
+		System.out.println("Third approach - searched element is : " + (sumOfTheArray(biggestArray)-sumOfTheArray(secondArray)));
+
+	}
+
+	public static Integer sumOfTheArray(Integer... array) {
+
+		int sum = 0;
+
+		for (Integer element : array) {
+			sum += element;
+		}
+
+		return sum;
 	}
 
 	public static void main(String[] ars) {
 
-		Integer[] array1 = { 1, 8, 7, 6, 3, 4, 9 };
+		Integer[] array1 = { 1, 8, 7, 6, 3, 4, 9, 2, -11};
 		Integer[] array2 = { 8, 7, 9, 4, 1, 6, 3, 2 };
 
 		if (array1.length > array2.length) {
@@ -55,6 +74,16 @@ public class FindUniqueElementFromTwoArrays {
 		} else {
 
 			findElementTheWorstApproach(array2, array1);
+		}
+
+		if (array1.length > array2.length) {
+
+			findElementUsingMap(array1, array2);
+
+		} else {
+
+			findElementUsingMap(array2, array1);
+
 		}
 
 	}
